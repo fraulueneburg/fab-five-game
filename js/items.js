97d1dc7e76764876821cbf414ba7bee8455ca924
fraulueneburg <github@fraulueneburg.de>
2023-09-13 11:59:23 +0200

    cleaned up items.js

--- js/items.js
+++ js/items.js
@@ -1,32 +1,24 @@
 let shapes = ['chair', 'bottle', 'book', 'ghost', 'mouse']
-let colorsLight = ['red', 'green', 'blue', 'white', 'grey']
-let colorsDark = ['pink', 'neongreen', 'cyan', 'yellow', 'purple']
 
-function createColorsArr(arr) {
-	return arr.map((colorName, index) => {
-		let num = (index + 1).toString().padStart(2, '0')
-		return {
-			name: `color${num}`,
-			alias: colorName,
-			cssColor: `var(--item${num}-color)`,
-		}
-	})
-}
+let colorsArr = shapes.map((shape, index) => {
+	let num = (index + 1).toString().padStart(2, '0')
+	return {
+		alias: `color${num}`,
+		cssColor: `var(--item${num}-color)`,
+	}
+})
 
-function createItemsArray(shapesArr, colorsArr) {
-	return shapesArr.map((shape, index) => {
+function createItemsArray(shapesArray, colorsArray) {
+	return shapesArray.map((shape, index) => {
 		let num = (index + 1).toString().padStart(2, '0')
 		return {
 			name: `item${num}`,
 			shape: shape,
-			color: colorsArr[index],
-			originalColor: colorsArr[index],
+			color: colorsArray[index],
+			originalColor: colorsArray[index],
 		}
 	})
 }
 
-let colorsLightArr = createColorsArr(colorsLight)
-let colorsDarkArr = createColorsArr(colorsDark)
-
-let itemsLightArr = createItemsArray(shapes, colorsLightArr)
-let itemsDarkArr = createItemsArray(shapes, colorsDarkArr)
+let itemsLightArr = createItemsArray(shapes, colorsArr)
+let itemsDarkArr = createItemsArray(shapes, colorsArr)
